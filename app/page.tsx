0ef2de2f20ac1c56947446c18a679a6b34e25007
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2025-03-21 08:26:11 -0400

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <div className="bg-white p-10">
+        <Button className="bg-slate-900">Utsav Patel</Button>
+      </div>
+    </>
+  );
+}
