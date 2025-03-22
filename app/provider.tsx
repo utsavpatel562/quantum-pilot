685666f415b97320e51a17028ac71fcb94430ffa
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2025-03-22 12:33:57 -0400

    latest commit

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 function Provider({
   children,
@@ -8,14 +9,16 @@ function Provider({
   children: React.ReactNode;
 }>) {
   return (
-    <NextThemesProvider
-      attribute="class"
-      defaultTheme="dark"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <div>{children}</div>
-    </NextThemesProvider>
+    <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
+      <NextThemesProvider
+        attribute="class"
+        defaultTheme="light"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <div>{children}</div>
+      </NextThemesProvider>
+    </GoogleOAuthProvider>
   );
 }
 
