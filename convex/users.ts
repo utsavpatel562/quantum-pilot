2e6759baf5950db1c6d3fbbd103f69504edb6851
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2025-03-24 11:40:29 -0400

    latest commit

--- convex/users.ts
+++ convex/users.ts
@@ -1,26 +1,33 @@
 import { v } from "convex/values";
 import { mutation } from "./_generated/server";
 
-export const CreateUser=mutation({
+// Define a mutation for creating a new user
+export const CreateUser  = mutation({
+    // Define the arguments that the mutation accepts
     args: {
         name: v.string(),
         email: v.string(),
         picture: v.string(),
     },
-    handler:async(ctx, args)=> {
-        // if user already exists in table
-        const user = await ctx.db.query('users').filter(q=>q.eq(q.field('email'), args.email)).collect();
-        if(user?.length==0) {
-            // If not exist then add new user
-            const data={
-                name: args.name,
-                email: args.email,
-                picture: args.picture,
-                credits: 5000,
-            }
+    // Define the handler function for the mutation
+    handler: async (ctx, args) => {
+        // Query the 'users' table to check if a user with the provided email already exists
+        const user = await ctx.db.query('users').filter(q => q.eq(q.field('email'), args.email)).collect();
+        
+        // Check if the user array is empty (i.e., the user does not exist)
+        if (user?.length == 0) {
+            // If the user does not exist, prepare the data for the new user
+            const data = {
+                name: args.name,        
+                email: args.email,      
+                picture: args.picture,  
+                credits: 5000,    
+            };
             const result = await ctx.db.insert('users', data);
+            // Return the newly created user data
             return data;
         }
+        // If the user already exists, return the existing user data
         return user[0];
     }
-})
+});
