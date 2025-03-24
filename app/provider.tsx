8fc08417a1ef7ff31a117b58040baa981514a879
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2025-03-24 11:19:18 -0400

    latest commit

--- app/provider.tsx
+++ app/provider.tsx
@@ -2,22 +2,26 @@
 import React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 
 function Provider({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
   return (
     <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
-      <NextThemesProvider
-        attribute="class"
-        defaultTheme="light"
-        enableSystem
-        disableTransitionOnChange
-      >
-        <div>{children}</div>
-      </NextThemesProvider>
+      <ConvexProvider client={convex}>
+        <NextThemesProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div>{children}</div>
+        </NextThemesProvider>
+      </ConvexProvider>
     </GoogleOAuthProvider>
   );
 }
