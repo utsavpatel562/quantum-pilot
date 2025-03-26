3ae1eb6701d67882d94c1a5d6faa3c744ad83a08
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2025-03-26 08:57:19 -0400

    latest commit

--- app/(main)/_components/Header.tsx
+++ app/(main)/_components/Header.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+"use client";
+import { AuthContext } from "@/context/AuthContext";
+import Image from "next/image";
+import React, { useContext } from "react";
 
 function Header() {
-  return <div>Header</div>;
+  const { user } = useContext(AuthContext);
+  return (
+    <>
+      <div className="p-3 shadow-sm">
+        <Image src={"/logo.svg"} alt="logo" width={40} height={40} />
+        <Image src={user?.image} alt="logo" width={40} height={40} />
+      </div>
+    </>
+  );
 }
 
 export default Header;
--- app/(main)/provider.tsx
+++ app/(main)/provider.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 import Header from "./_components/Header";
+import { GetAuthUserData } from "@/services/GlobalApi";
+import { useRouter } from "next/navigation";
 
 function Provider({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const router = useRouter();
+  const CheckUserAuth = async () => {
+    const token = localStorage.getItem("user_token");
+    // Get New Access Token
+    const user = token && (await GetAuthUserData(token));
+    if (!user) {
+      router.replace("sign-in");
+      return;
+    }
+    // Get User Info from Database
+  };
   return (
     <div>
       <Header />
