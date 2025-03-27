d6857ca361c7bcd0e75cdf6f835a251345c8bb6d
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2025-03-27 12:17:04 -0400

    latest commit

---
+++ services/AiAssistantsList.tsx
@@ -0,0 +1,44 @@
+export default [
+  {
+    id: 1,
+    name: "",
+    title: "Fitness Coach",
+    image: "/fitness_coach.png",
+    instruction: `Answer fitness-realated questions only`,
+    userInstruction: `Respond to any fitness-related query.`,
+    sampleQuestions: [
+      "What's the workout for building muscle?",
+      "How can I lose weight effectively?",
+      "What should I eat before and after a workout?",
+      "Can you suggest a home workout routine?",
+    ],
+  },
+  {
+    id: 2,
+    name: "",
+    title: "Grammar Fixer",
+    image: "/grammer_fixer.png",
+    instruction: `Fix grammar mistakes in provided text.`,
+    userInstruction: `Check the grammar of the provided text and correct it if necessary.`,
+    sampleQuestions: [
+      "Can you fix the grammar in this sentence?",
+      "Is this sentence grammatically correct?",
+      "Can you check my email for grammar mistakes?",
+      "What's the correct way to use a semicolon?",
+    ],
+  },
+  {
+    id: 3,
+    name: "",
+    title: "Email Writer & Reply Assistant",
+    image: "/img4.png",
+    instruction: `Assist in writing and replying to emails.`,
+    userInstruction: `Generate well-structured emails based on the provided information.`,
+    sampleQuestions: [
+      "Can you write a formal email for a job application?",
+      "How can I politely decline an invitation on email?",
+      "Can you help me to write a follow-up email?",
+      "How do I start an email to a professor?",
+    ],
+  },
+];
