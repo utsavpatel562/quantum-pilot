cd231e239b2eada3c7877b52586de14b03839dd5
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2025-04-02 12:01:31 -0400

    latest commit

---
+++ app/(main)/workspace/_components/AssistantList.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function AssistantList() {
+  return (
+    <>
+      <div className="p-5 bg-secondary border-r-[1px] h-screen">
+        <h2 className="font-bold text-lg">Your Personal AI Assistants</h2>
+      </div>
+    </>
+  );
+}
+
+export default AssistantList;
--- app/(main)/workspace/page.tsx
+++ app/(main)/workspace/page.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import AssistantList from "./_components/AssistantList";
 
 function WorkSpace() {
-  return <div>WorkSpace</div>;
+  return (
+    <>
+      <div>
+        <div className="grid grid-cols-5">
+          {/*Assistant List - Selected by User*/}
+          <div className="hidden md:block">
+            <AssistantList />
+          </div>
+          {/*Chat UI*/}
+          <div className="lg:col-span-3 md:col-span-4">Chat UI</div>
+          {/*Settings*/}
+          <div className="hidden lg:block">Settings</div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default WorkSpace;
