217059ebfdf55fe9265c96eb18f1845f5010afe5
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2025-04-03 08:16:21 -0400

    latest commit

--- app/(main)/workspace/_components/AssistantList.tsx
+++ app/(main)/workspace/_components/AssistantList.tsx
@@ -4,11 +4,14 @@ import { Input } from "@/components/ui/input";
 import { AuthContext } from "@/context/AuthContext";
 import { api } from "@/convex/_generated/api";
 import { useConvex } from "convex/react";
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { ASSISTANT } from "../../ai-assistants/page";
+import Image from "next/image";
 
 function AssistantList() {
   const { user } = useContext(AuthContext);
   const convex = useConvex();
+  const [assistantList, setAssistantList] = useState<ASSISTANT[]>([]);
   useEffect(() => {
     user && GetUserAssistants();
   }, [user]);
@@ -21,6 +24,7 @@ function AssistantList() {
       }
     );
     console.log(result);
+    setAssistantList(result);
   };
   return (
     <>
@@ -32,6 +36,29 @@ function AssistantList() {
           + Add New AI Agent
         </Button>
         <Input className="bg-white mt-3 rounded-sm p-5" placeholder="Search" />
+        <div className="mt-5">
+          {assistantList.map((assistant, index) => (
+            <div className="flex p-2 gap-3 items-center" key={index}>
+              <Image
+                src={assistant.image}
+                alt={assistant.name}
+                width={60}
+                height={60}
+                className="object-cover border border-gray-300 shadow-md rounded-md w-[70px] h-[70px]"
+                style={{
+                  background:
+                    "linear-gradient(to top, #6a85b6 0%, #bac8e0 100%)",
+                }}
+              />
+              <div>
+                <h2 className="font-bold">{assistant.name}</h2>
+                <h2 className="text-gray-600 dark:text-gray-400">
+                  {assistant.title}
+                </h2>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
